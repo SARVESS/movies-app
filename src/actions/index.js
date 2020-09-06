82f895866e7543d5c8f0aa1b075862664451b288
SARVESS <sarveshsri76@gmail.com>
2020-09-06 18:11:34 +0530

    Changing state to add favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,14 @@
 // {
 //     type: 'DECREASE_COUNT'
 // }
+
+// actions types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies (movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
+}
