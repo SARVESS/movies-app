e9a84076647f1ce3c11f94487b7f19ab29ac34d6
SARVESS <sarveshsri76@gmail.com>
2020-09-08 01:10:23 +0530

    Context Api

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { addMovies, setShowFavourites } from '../actions';
+import { StoreContext } from '../index';
 
 class App extends React.Component {
   componentDidMount() {
@@ -19,7 +20,7 @@ class App extends React.Component {
   }
 
   isMovieFavourite = (movie) => {
-    const { movies } = this.props.store.getState(); 
+    const { movies } = this.props.store.getState();
     const index = movies.favourites.indexOf(movie);
 
     if (index !== -1) {
@@ -29,21 +30,33 @@ class App extends React.Component {
     return false;
   };
   onChangeTab = (val) => {
-    this.props.store.dispatch(setShowFavourites(val))
-  }
+    this.props.store.dispatch(setShowFavourites(val));
+  };
   render() {
     const { movies, search } = this.props.store.getState(); // {movies: {}, seacrh: {} }
-    const { list, favourites, showFavourites } =  movies;
+    const { list, favourites, showFavourites } = movies;
     console.log('RENDER', this.props.store.getState());
 
     const displayMovies = showFavourites ? favourites : list;
     return (
       <div className="App">
-        <Navbar dispatch={this.props.store.dispatch} search={search}/>
+        <Navbar search={search} />
         <div className="main">
           <div className="tabs">
-            <div className={`tab ${showFavourites ? '' : 'active-tabs'}`} onClick={() => this.onChangeTab(false)}> Movies </div>
-            <div className={`tab ${showFavourites ? 'active-tabs': ''}`} onClick={() => this.onChangeTab(true)}> Favourites </div>
+            <div
+              className={`tab ${showFavourites ? '' : 'active-tabs'}`}
+              onClick={() => this.onChangeTab(false)}
+            >
+              {' '}
+              Movies{' '}
+            </div>
+            <div
+              className={`tab ${showFavourites ? 'active-tabs' : ''}`}
+              onClick={() => this.onChangeTab(true)}
+            >
+              {' '}
+              Favourites{' '}
+            </div>
           </div>
           <div className="list">
             {displayMovies.map((movie, index) => (
@@ -55,11 +68,23 @@ class App extends React.Component {
               />
             ))}
           </div>
-          {displayMovies.length === 0 ? <div className="no-movies">No Movies to display!</div>: null}
+          {displayMovies.length === 0 ? (
+            <div className="no-movies">No Movies to display!</div>
+          ) : null}
         </div>
       </div>
     );
   }
 }
 
-export default App;
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <StoreContext.Consumer>
+        {(store) => <App store={store} />}
+      </StoreContext.Consumer>
+    );
+  }
+}
+
+export default AppWrapper;
