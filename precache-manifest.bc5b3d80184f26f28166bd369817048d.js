68e97451508498146aee5f5efe0496b700621394
SARVESS <sarveshsri76@gmail.com>
2020-09-08 15:41:32 +0530

    Updates

---
+++ precache-manifest.bc5b3d80184f26f28166bd369817048d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e7999ae14d61f75d1c7023e4772f7f",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "17c656af59770d34c0a9",
+    "url": "/movies-app/static/css/main.306cb92d.chunk.css"
+  },
+  {
+    "revision": "a793bb7fa43ecc5ce7f4",
+    "url": "/movies-app/static/js/2.432ab593.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-app/static/js/2.432ab593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c656af59770d34c0a9",
+    "url": "/movies-app/static/js/main.05e2927a.chunk.js"
+  },
+  {
+    "revision": "911c51c8a56e1ed2764c",
+    "url": "/movies-app/static/js/runtime-main.5a47ebf3.js"
+  }
+]);
